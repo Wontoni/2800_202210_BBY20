3ebe03c29b9c804bb760f26f524616b3512a2b82
kohdc1723 <root@Dongchan-Koh-MacBookAir.local>
2022-05-01 20:34:34 -0700

    set up folder structure

---
+++ app/server.js
@@ -0,0 +1,67 @@
+"use strict";
+// port number
+const PORT = 2800;
+// html files directory
+const htmlDir = __dirname + "/views";
+// express module
+const express = require("express");
+const app = express();
+// set static files
+app.use("/public", express.static("public"));
+// file system module
+const fs = require("fs");
+app.set('view engine', 'ejs');
+// body-parser module
+const bodyParser = require("body-parser");
+const { response } = require("express");
+app.use(bodyParser.urlencoded({ extended: true }));
+// mongoDB setting
+const MongoClient = require("mongodb").MongoClient;
+var db;
+MongoClient.connect("mongodb+srv://bby20:unified20@cluster0.wphdm.mongodb.net/Unified?retryWrites=true&w=majority", (error, client) => {
+    if (error) {
+        return console.log(error);
+    } else {
+        db = client.db("Unified");
+        
+        app.listen(PORT, () => {
+            console.log("Server is operating on port 2800");
+        });
+    }
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(htmlDir + "/index.html");
+});
+
+app.get("/home", (req, res) => {
+    res.sendFile(htmlDir + "/home.html");
+});
+
+app.get("/sign-up", (req, res) => {
+    res.sendFile(htmlDir + "/sign-up.html");
+});
+
+app.get("/home", (req, res) => {
+    res.sendFile(htmlDir + "/home.html");
+});
+
+app.post("/register", (req, res) => {
+    db.collection("user").insertOne({
+        username: req.body.username,
+        password: req.body.password
+    }, (error, result) => {
+        if (result.acknowledged) {
+            res.redirect("/");
+        }
+    });
+});
+
+app.get("/admin", (req, res) => {
+    db.collection("user").find().toArray((error, result) => {
+        console.log(result);
+        res.sendFile(htmlDir + "/admin.html", {
+            users: result
+        });
+    });
+});
