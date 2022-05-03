a791a32be518d95aa06f7b8b08839b15e101d9a8
Skylar Park <pjy990625@gmail.com>
2022-05-02 21:01:51 -0700

    added a function that add users to database

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 "use strict";
+
 // port number
 const PORT = 2800;
 // html files directory
@@ -11,15 +12,16 @@ app.use("/public", express.static("public"));
 // body-parser module
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+
 // mongoDB setting
 const MongoClient = require("mongodb").MongoClient;
+const URL = "mongodb+srv://bby20:unified20@cluster0.wphdm.mongodb.net/Unified?retryWrites=true&w=majority";
 var db;
-MongoClient.connect("mongodb+srv://bby20:unified20@cluster0.wphdm.mongodb.net/Unified?retryWrites=true&w=majority", (error, client) => {
+MongoClient.connect(URL, (error, client) => {
     if (error) {
         return console.log(error);
     } else {
         db = client.db("Unified");
-        
         app.listen(PORT, () => {
             console.log("Server is operating on port 2800");
         });
@@ -38,14 +40,24 @@ app.get("/home", (req, res) => {
     res.sendFile(htmlDir + "/home.html");
 });
 
-app.post("/register", (req, res) => {
-    db.collection("user").insertOne({
-        username: req.body.username,
-        password: req.body.password
-    }, (error, result) => {
-        if (result.acknowledged) {
-            res.redirect("/");
-        }
+app.post('/register', (req, res) => {
+    db.collection('count').findOne({ name: 'NumberOfUsers' }, (error, result) => {
+        var totalUsers = result.totalUser;
+        // add a user
+        db.collection('user').insertOne({
+            _id: totalUsers + 1,
+            username: req.body.username,
+            password: req.body.password,
+            role: req.body.input_check
+        }, (error, result) => {
+            // increment the total number of users
+            console.log('saved successfully');
+            db.collection('count').updateOne({ name: 'NumberOfUsers' }, { $inc: { totalUser: 1 } }, (error, result) => {
+                if (result.acknowledged) {
+                    res.redirect("/");
+                }
+            });
+        });
     });
 });
 
