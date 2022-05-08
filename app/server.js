35cff87f4285bbcc9c65b1980b4be3bc027da42c
kohdc1723 <root@Dongchan-Koh-MacBookAir.local>
2022-05-07 22:26:09 -0700

    logged in user cannot go back to login page, move html files to public folder

--- app/server.js
+++ app/server.js
@@ -8,12 +8,26 @@ const express = require("express");
 const app = express();
 // body-parser
 const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // security code
 app.use(function(req, res, next) {
     res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
     next();
 });
+
+/* ------------------------------ DB Setting ------------------------------ */
+const MongoClient = require("mongodb").MongoClient;
+const URL = "mongodb+srv://bby20:unified20@cluster0.wphdm.mongodb.net/Unified?retryWrites=true&w=majority";
+let db;
+MongoClient.connect(URL, (error, client) => {
+    if (error) {
+        return console.log(error);
+    } else {
+        db = client.db("Unified");
+    }
+});
+app.db = db;
 
 /* ------------------------------ Static Path ------------------------------ */
 app.use("/public", express.static("public"));
@@ -27,5 +41,5 @@ app.use("/", authRoute);
 
 /* ------------------------------ Listen to Server ------------------------------ */
 app.listen(PORT, () => {
-    console.log("Server is operating on port 2800");
+    console.log("Server is operating on port 8000");
 });
