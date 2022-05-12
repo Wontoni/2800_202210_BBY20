1fc9cb14a1e4d79ba4e457210be4134d6d38d4f9
Winston Nguyen <winston_nguyenn@hotmail.com>
2022-05-12 11:52:20 -0700

    updated tips popups

--- app/public/js/tipsPop.js
+++ app/public/js/tipsPop.js
@@ -1,22 +1,12 @@
 "use strict";
 
-$("#readMore").click(function (){
-    $("#readMore").toggleClass("hide");
-    $("#readMoreContent").toggleClass("hide");
-    $("#readLess").toggleClass("hide");
-})
-
-$("#readLess").click(function (){
-    $("#readMore").toggleClass("hide");
-    $("#readMoreContent").toggleClass("hide");
-    $("#readLess").toggleClass("hide");
-})
-
 function getRandomInt(upperBound) {
     // Exclusive upperBound
     return Math.floor(Math.random() * upperBound);
-  }
+}
 
+
+// Change this array to an array of items in the quick tips table
 let tips = [
     ["Get into a routine", "Getting into a routine lets you get used to studying, and helps make the transition to post-secondary much easier!"],
     ["Use class time wisely", "To maximize what you learn, you should be taking notes, reading lectures before hand, and creating flash cards!"],
@@ -24,7 +14,7 @@ let tips = [
     ["Meditate or Exercise", "Meditating and exercising are a great way to keep your body healthy, and several studies say they help with studying as well!"]
 ]
 
-$("#createPopup").click(function(){
+function createPopup() {
     let tipNo = getRandomInt(tips.length);
     let tipTitle = tips[tipNo][0];
     let tipDesc = tips[tipNo][1];
@@ -32,7 +22,15 @@ $("#createPopup").click(function(){
     let popupDiv = document.createElement("div");
     popupDiv.setAttribute('class', "tipsPop")
 
-    let x = document.createTextNode("Tip of the day: ")
+    let closeBtn = document.createElement("span");
+    closeBtn.setAttribute("class", "material-icons closePop");
+    let closeText = document.createTextNode("close");
+    closeBtn.appendChild(closeText);
+
+    let divTitle = document.createElement("div");
+    divTitle.setAttribute('class', 'tipTitle');
+    let x = document.createTextNode("Quick Tip")
+    divTitle.appendChild(x);
 
     let tipTitleDiv = document.createElement("div");
     tipTitleDiv.setAttribute('id', 'TOFD');
@@ -56,10 +54,39 @@ $("#createPopup").click(function(){
     let seeLessText = document.createTextNode("See less");
     tipSeeLess.appendChild(seeLessText);
 
-    popupDiv.appendChild(x);
+    popupDiv.appendChild(closeBtn);
+    popupDiv.appendChild(divTitle);
     popupDiv.appendChild(tipTitleDiv);
     popupDiv.appendChild(tipSeeMore);
     popupDiv.appendChild(tipDescDiv);
     popupDiv.appendChild(tipSeeLess);
     document.body.appendChild(popupDiv);
-})
+
+    // To open readMoreContent
+    $("#readMore").click(function () {
+        $("#readMore").toggleClass("hide");
+        $("#readMoreContent").toggleClass("hide");
+        $("#readLess").toggleClass("hide");
+        $("#TOFD").toggleClass("hide");
+    })
+
+    // To close readMoreContent
+    $("#readLess").click(function () {
+        $("#readMore").toggleClass("hide");
+        $("#readMoreContent").toggleClass("hide");
+        $("#readLess").toggleClass("hide");
+        $("#TOFD").toggleClass("hide");
+    })
+
+    // To close popup
+    $(".closePop").click(function () {
+        $(".tipsPop").fadeOut();
+    })
+}
+var delayMilli = 2000;
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        createPopup();
+    }, delayMilli);
+});
+
