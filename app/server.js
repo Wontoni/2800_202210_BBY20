322178700584655ca323688b301211b05ffee2cd
Winston <winston_nguyenn@hotmail.com>
2022-05-13 10:42:17 -0700

    Created files for heroku deploy

--- app/server.js
+++ app/server.js
@@ -101,3 +101,19 @@ app.use("/profile", profileRoute);
 
 /* ------------------------------ Listen to Server ------------------------------ */
 app.listen(PORT);
+
+/* ------------------------------ Heroku Server Hosting ------------------------------ */
+let http = require('http');
+let url = require('url');
+
+http.createServer((req, res) => {
+    let q = url.parse(req.url, true);
+    console.log(q.query);
+
+    res.writeHead(200, {
+        "Content-Type": "text/html",
+        "Access-Control-Allow-Origin": "*"
+    });
+
+    res.end(`Hello ${q.query['name']}`);
+}).listen(process.env.PORT || 8000);
