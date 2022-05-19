9148d8af0fa505d38b0d43d09eff63cc29df63e1
Skylar Park <pjy990625@gmail.com>
2022-05-19 01:13:10 -0700

    fixed delete error

--- app/public/js/timeline.js
+++ app/public/js/timeline.js
@@ -13,7 +13,7 @@ $('.delete').click((e) => {
     });
 });
 
-$('.edit').click((e) => {
+$('.edit-button').click((e) => {
     var postNumber = e.target.dataset.number;
     $.ajax({
         method: 'GET',
