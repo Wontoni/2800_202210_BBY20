be5f2e7b740cc5577a29e25e4cf46cee2989a5eb
Winston <winston_nguyenn@hotmail.com>
2022-05-18 21:23:14 -0700

    Created sliding div for easter egg function

---
+++ app/public/js/easter.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var moveSpeed = 9000; // Milliseconds -- Animation speed
+var delayAnimation = 1000; // Milliseconds -- Initial delay time
+
+//Randomize animation delay in milliseconds
+const lowerBound = 20000
+const upperBound = 60000
+const difference = upperBound - lowerBound;
+
+// Start easter egg sliding
+const delayStart = setTimeout(start, delayAnimation);
+
+// Move divv off the right of the screen
+function moveRight() {
+    setTimeout(() => {
+        $("#easterEgg").animate({
+            left: "3000px"
+        }, moveSpeed, function() {
+            moveLeft();
+        });
+    }, delayAnimation
+)}
+
+// Move div off the left of the screen
+function moveLeft() {
+    delayAnimation = Math.floor(Math.random() * difference) + lowerBound;
+    console.log(delayAnimation);
+    setTimeout(() => {
+        $("#easterEgg").animate({
+            left: "-200px"
+        }, moveSpeed, function() {
+            delayAnimation = Math.floor(Math.random() * difference) + lowerBound;
+            moveRight();
+        });
+    }, delayAnimation
+)}
+
+function start() {
+    $("#easterEgg").css("display", "block");
+    moveRight();
+}
