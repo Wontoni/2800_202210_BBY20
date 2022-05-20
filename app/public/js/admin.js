fdd0627fad5961a1ab71cf53cfe998ed20f0f7c4
kohdc1723 <root@Dongchan-Koh-MacBookAir.local>
2022-05-20 00:52:35 -0700

    username check when creating account in admin dashboard

--- app/public/js/admin.js
+++ app/public/js/admin.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 $('.delete').click((e) => {
     var userNumber = e.target.dataset.number;
     console.log(userNumber);
@@ -25,5 +24,28 @@ $('.edit').click((e) => {
         window.location.href = `/edit/${userNumber}`;
     }).fail((error) => {
         console.log("error");
+    });
+});
+
+document.getElementById("create-button").addEventListener("click", () => {
+    fetch("/create", {
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({
+            "username" : document.getElementById("userName").value,
+            "email" : document.getElementById("userEmail").value,
+            "password" : document.getElementById("userPassword").value
+        })
+    }).then((res) => {
+        return res.json();
+    }).then((data) => {
+        document.getElementById("userName").value = null;
+        document.getElementById("userEmail").value = null;
+        document.getElementById("userPassword").value = null;
+        document.getElementById("create-message").innerHTML = data.message;
+    }).catch(() => {
+        window.location.href = "/main";
     });
 });
--- app/routes/admin-router.js
+++ app/routes/admin-router.js
@@ -66,28 +66,43 @@ router.delete('/delete', (req, res) => {
 
 // create a user
 router.post('/create', (req, res) => {
-    db.collection('BBY_20_Count').findOne({ name: 'NumberOfUsers' }, (error, result) => {
-        // add a user
-        let totalUsers = result.totalUser;
-        const defaultSchool = "";
-        const defaultAvatarURL = "public/assets/upload/default-avatar.png";
-        const defaultRole = "regular";
-        db.collection('BBY_20_User').insertOne({
-            _id: totalUsers + 1,
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password,
-            school: defaultSchool,
-            avatar: defaultAvatarURL,
-            role: defaultRole
-        }, (error, result) => {
-            // increment the total number of users
-            db.collection('BBY_20_Count').updateOne({ name: 'NumberOfUsers' }, { $inc: { totalUser: 1 } }, (error, result) => {
-                if (result.acknowledged) {
-                    res.redirect("/main");
-                }
+    db.collection("BBY_20_User").findOne({
+        username: req.body.username
+    }, (error, result) => {
+        let exist = false;
+        if (result) {
+            exist = true;
+        }
+
+        if (exist) {
+            res.json({
+                message: "This username already exists"
             });
-        });
+        } else {
+            db.collection('BBY_20_Count').findOne({ name: 'NumberOfUsers' }, (error, result) => {
+                // add a user
+                let totalUsers = result.totalUser;
+                const defaultSchool = "";
+                const defaultAvatarURL = "public/assets/upload/default-avatar.png";
+                const defaultRole = "regular";
+                db.collection('BBY_20_User').insertOne({
+                    _id: totalUsers + 1,
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    school: defaultSchool,
+                    avatar: defaultAvatarURL,
+                    role: defaultRole
+                }, (error, result) => {
+                    // increment the total number of users
+                    db.collection('BBY_20_Count').updateOne({ name: 'NumberOfUsers' }, { $inc: { totalUser: 1 } }, (error, result) => {
+                        if (result.acknowledged) {
+                            res.redirect("/main");
+                        }
+                    });
+                });
+            });
+        }
     });
 });
 
