f778406f855a2dbf4ecd2d1511117ee208f56450
Winston <winston_nguyenn@hotmail.com>
2022-05-20 04:50:29 -0700

    Made friends list clickable

--- app/public/js/friends.js
+++ app/public/js/friends.js
@@ -5,9 +5,29 @@ window.addEventListener('load', function () {
     var friendsList = document.querySelector('.friendsList');
     var messages = document.querySelector('.messageContainer');
 
-    friendsIcon.onclick = function() {
+    var friendItem = document.querySelector('.friendItem');
+
+    friendsIcon.onclick = function () {
         friendsIcon.classList.toggle('close');
         friendsList.classList.toggle('close-friends');
         messages.classList.toggle("close")
     }
+
+    $('.friendItem').click(function () {
+        $(".friendItem").removeClass("active");
+        $(this).attr("class", "friendItem active");
+    })
+    $('#addFriendSearch').keyup( function() {
+        if (this.value.length !== 0) {
+
+            // Change to flex if a horizontal list is desired
+            $(".allUsers").css("display", "block");
+        }
+    })
+
+    $('#addFriendSearch').keyup( function() {
+        if (this.value.length === 0) {
+            $(".allUsers").css("display", "none");
+        }
+    })
 })
