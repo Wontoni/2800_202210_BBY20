8ee6b7cf5e7195c8cea5ff95e745b7b38dfe411b
Winston <winston_nguyenn@hotmail.com>
2022-05-20 11:37:29 -0700

    Added confirm css and js files

---
+++ app/public/js/confirm.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var pop = document.getElementById("confirmPopup");
+
+// Open confirm popup
+$('.button').click(function (e) {
+    pop.style.display = "flex";
+
+    var dataNum = e.target.dataset.number;
+    $('#confirmBtn').attr('data-number', dataNum);
+    console.log(dataNum);
+})
+
+// Close confirm popup
+$('.closeBtn').click(function () {
+    pop.style.display = "none";
+})
+$('.delete').click(function () {
+    pop.style.display = "none";
+})
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == pop) {
+        pop.style.display = "none";
+    }
+}
