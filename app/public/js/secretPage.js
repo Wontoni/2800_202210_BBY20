10ce3b74839fa9d680b4cc1859261f047249c0f5
Winston <winston_nguyenn@hotmail.com>
2022-05-20 11:24:07 -0700

    Added easter  egg (Without route)

---
+++ app/public/js/secretPage.js
@@ -0,0 +1,25 @@
+"use strict";
+
+window.addEventListener('load', function () {
+
+})
+
+function checkWord() {
+    var text = document.querySelector('#secret-word').value;
+
+    var wrongWord = document.querySelector('#wrong-word');
+
+
+    if (text.toLowerCase() === "college") {
+        location.href = "https://www.youtube.com/watch?v=DLzxrzFCyOs";
+    } else {
+        wrongWord.classList.toggle('hide');
+        document.querySelector('#secret-word').value = "";
+    }
+
+}
+
+function closePopup() {
+    var wrongWord = document.querySelector('#wrong-word');
+    wrongWord.classList.toggle('hide');
+}
