5084c08667c83474ec0354a0793fb74a31463abb
Winston <winston_nguyenn@hotmail.com>
2022-05-20 02:24:43 -0700

    Created tips database and delete/edit functions

--- app/routes/admin-router.js
+++ app/routes/admin-router.js
@@ -33,7 +33,9 @@ const directory = {
     profile: path.join(__dirname, "../public/html", "profile.html"),
     admin: path.join(__dirname, "../public/html", "admin.html"),
     upload: path.join(__dirname, "../public/assets/upload/"),
-    edit: path.join(__dirname, "../public/html", "edit.html")
+    edit: path.join(__dirname, "../public/html", "edit.html"),
+    tips: path.join(__dirname, "../public/html", "admin_tips.html"),
+    editTips: path.join(__dirname, "../public/html", "edit-tips.html")
 };
 
 /* ------------------------------ Routers ------------------------------ */
@@ -144,6 +146,129 @@ router.put("/user-edit", (req, res) => {
         });
     });
 });
+
+
+
+// Show tips page
+router.get("/tips", (req, res) => {
+    if (!req.user) {
+        res.sendFile(directory.login);
+    } else {
+
+        const tips = fs.readFileSync(directory.tips);
+        const tipsHTML = new JSDOM(tips);
+        tipsHTML.window.document.getElementById("username").innerHTML = req.user.username;
+        var tipsTemplate = tipsHTML.window.document.getElementById("tipsTemplate");
+        var listTemplate = tipsHTML.window.document.getElementById("listTemplate");
+        db.collection("BBY_20_Tips").find().toArray((error, result) => {
+            for (var i = 0; i < result.length; i++) {
+                var number = result[i]._id;
+                var title = result[i].title;
+                var desc = result[i].description;
+
+                var tipsInfo = tipsTemplate.cloneNode(true);
+                tipsTemplate.remove();
+                tipsInfo.querySelector("#delete-number").setAttribute("data-number", `${number}`);
+                tipsInfo.querySelector("#edit-number").setAttribute("data-number", `${number}`);
+                tipsInfo.querySelector("#title").innerHTML = title;
+                tipsInfo.querySelector("#description").innerHTML = desc;
+
+                listTemplate.appendChild(tipsInfo);
+            }
+            tipsHTML.window.document.getElementById("total-tips").innerHTML = result.length + " tips";
+            res.send(tipsHTML.serialize());
+        });
+
+    }
+});
+
+// delete a tip
+router.delete('/delete-tip', (req, res) => {
+    req.body._id = parseInt(req.body._id);
+    db.collection('BBY_20_Tips').findOne({ _id: req.body._id }, (error, result) => {
+
+        db.collection('BBY_20_Count').findOne({ name: 'NumberOfTips' }, (error, result) => {
+
+            db.collection('BBY_20_Tips').deleteOne(req.body, (error, result) => {
+                // decrement the total number of tips
+                db.collection('BBY_20_Count').updateOne({ name: 'NumberOfTips' }, { $inc: { totalTips: -1 } }, (error, result) => {
+                    res.sendFile(directory.tips);
+                });
+            });
+
+        });
+    });
+});
+
+// create a tip
+router.post('/tip-create', (req, res) => {
+    db.collection("BBY_20_Tips").findOne({
+        title: req.body.title
+    }, (error, result) => {
+        let exist = false;
+        if (result) {
+            exist = true;
+        }
+
+        if (exist) {
+            res.json({
+                message: "This tips already exists"
+            });
+        } else {
+            db.collection('BBY_20_Count').findOne({ name: 'NumberOfTips' }, (error, result) => {
+                // add a tip
+                let totalTips = result.totalTips;
+
+                db.collection('BBY_20_Tip').insertOne({
+                    _id: totalTips + 1,
+                    title: req.body.title,
+                    description: req.body.description,
+
+                }, (error, result) => {
+                    // increment the total number of users
+                    db.collection('BBY_20_Count').updateOne({ name: 'NumberOfTips' }, { $inc: { totalTips: 1 } }, (error, result) => {
+                        if (result.acknowledged) {
+                            res.redirect("/tips");
+                        }
+                    });
+                });
+            });
+        }
+    });
+});
+
+// show edit tip page
+router.get("/edit-tips/:id", (req, res) => {
+    if (!req.user) {
+        res.sendFile(directory.login);
+    } else {
+        const editTips = fs.readFileSync(directory.editTips);
+        const editTipsHTML = new JSDOM(editTips);
+        db.collection('BBY_20_Tips').findOne({ _id: parseInt(req.params.id) }, (error, result) => {
+            editTipsHTML.window.document.getElementById("tipNumber").setAttribute("value", `${result._id}`);
+            editTipsHTML.window.document.getElementById("tipTitle").setAttribute("value", `${result.title}`);
+            editTipsHTML.window.document.getElementById("tipDescription").setAttribute("value", `${result.description}`);
+            res.send(editTipsHTML.serialize());
+        });
+    }
+});
+
+// edit tip information
+router.put("/tip-edit", (req, res) => {
+    req.body._id = parseInt(req.body._id);
+    db.collection('BBY_20_Tips').updateOne({ _id: req.body._id }, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description
+        }
+    }, (error, result) => {
+        res.redirect("/tips");
+    });
+});
+
+
+
+
 
 /* ------------------------------ Export Module ------------------------------ */
 module.exports = router;
